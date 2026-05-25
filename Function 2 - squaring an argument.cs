97d4d350ff6afee7485672b379ffbcc44736b734
AkhrorKhasanov <ahrorhasanov815@gmail.com>
2026-05-25 08:09:35 +0500

    25.05.2026 08:09

---
+++ Function 2 - squaring an argument.cs
@@ -0,0 +1,9 @@
+public class Kata
+{
+  // Write a static function "Square" here which takes a double
+  // and returns the square of that double
+  public static double Square(double num)
+  {
+    return num * num;
+  }
+}
